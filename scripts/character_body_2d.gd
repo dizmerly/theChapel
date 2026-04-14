0c498345c9cbfeda1ffa201024cc8856379fe955
Daniel Izmerly <dan.izmerly@gmail.com>
2026-04-13 22:42:38 -0500

    assets

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -5,7 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add the gravity.	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -16,6 +16,9 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	# make sure to flip the sprite here if going left/right
+	
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
